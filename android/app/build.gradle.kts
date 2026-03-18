aa62620785e5c53751b7cb09f0e4759304eb6e04
altman <altman@gitlab.local>
2026-03-18 16:40:12 +0800

    fix: 修改ndk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.wallet"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = 28.2.13676358
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
